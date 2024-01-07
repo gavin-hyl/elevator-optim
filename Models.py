06aeeacacf128387692bda0028f184fbfe93ae65
Sandy <sandyli202101@gmail.com>
2024-01-06 18:14:26 -0600

    update scan

--- Models.py
+++ Models.py
@@ -93,11 +93,15 @@ def scan_helper(location: int, highest_floor: int, destinations: list[bool], out
     # if the elevator reaches the highest / lowest floor
     else:
         # if button pressed
-        if (outside_calls[location] or destinations[location]):
+        if (destinations[location]):
             if (location == 0):
                 return Constants.OPEN_UP
             else:
                 return Constants.OPEN_DOWN
+        elif (outside_calls[2 * location] and location == 0):
+            return Constants.OPEN_UP
+        elif (outside_calls[2 * location + 1] and location == highest_floor):
+            return Constants.OPEN_DOWN
         # if button is not pressed, reverse the direction
         elif (location == 0):
             # if reachable calls exist, move to that floor
